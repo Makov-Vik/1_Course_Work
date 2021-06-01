22c5090312d9cef9deccaada17a313f2d10b03a6
Makiv-Vik <71525436+Makiv-Vik@users.noreply.github.com>
2021-06-01 15:58:36 +0300

    add eslint

--- JS/chifrovanie.js
+++ JS/chifrovanie.js
@@ -1,30 +1,30 @@
 'use strict';
-const btn = document.getElementById('copyText');
-const btnforkey = document.getElementById('copyKey');
-const uncopy = document.getElementById('uncopyText');
-const text_input = document.getElementById('text_input');
-const text_input1 = document.getElementById('text_input1');
-const key_input = document.getElementById('key_input');
-const text_out = document.getElementById('text_out');
-const text_outkey = document.getElementById('text_out1');
-const text_unencrypt = document.getElementById('unencrypt');
-const button = document.getElementById('button');
-const rad = document.getElementsByName('dzen');
+const btn = this.document.getElementById('copyText');
+const btnforkey = this.document.getElementById('copyKey');
+const uncopy = this.document.getElementById('uncopyText');
+const text_input = this.document.getElementById('text_input');
+const text_input1 = this.document.getElementById('text_input1');
+const key_input = this.document.getElementById('key_input');
+const text_out = this.document.getElementById('text_out');
+const text_outkey = this.document.getElementById('text_out1');
+const text_unencrypt = this.document.getElementById('unencrypt');
+const button = this.document.getElementById('button');
+const rad = this.document.getElementsByName('dzen');
 text_out.innerHTML = '';
-const characterSpan = document.createElement('span');
-const characterSpan3 = document.createElement('span');
+const characterSpan = this.document.createElement('span');
+const characterSpan3 = this.document.createElement('span');
 const allsymbols = '1234567890-=+[];/.<>~@#$^&()_!L:HAGWYIMNBCVXZSJ*"?№×÷|';
-const string = allsymbols.split('');   
+const string = allsymbols.split('');
 text_unencrypt.innerHTML = '';
-const characterSpan2 = document.createElement('span');
+const characterSpan2 = this.document.createElement('span');
 const checking = (expression) => {
   const english = /^[a-zA-Z0-9]+$/;
   const aftercheck = expression.split('');
-  for (let i = 0; i < aftercheck.length; i++) {
+  for (let i = 0; i < aftercheck.length; i += 1) {
     if (english.test(aftercheck[i])) {
       window.alert('Ангийский не подходит' );
       throw e;
-    } 
+    }
   }
   if (expression === null || expression === '') {
     alert('Вы ничего не ввели');
@@ -34,23 +34,23 @@ const checking = (expression) => {
 
 function randomInteger(min, max) {
   // получить случайное число от (min-0.5) до (max+0.5)
-  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  const rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand);
 }
-const randomsymbol = () =>{//функция генерации символа
-  const rand = randomInteger(1, string.length-1);
+const randomsymbol = () => { // функция генерации символа
+  const rand = randomInteger(1, string.length - 1);
   const symbol = string[rand];
-  string.splice(rand, 1); 
+  string.splice(rand, 1);
   return symbol;
 };
-const generatekey = () =>{//функия генерирования ключа
-  let key = [];
+const generatekey = () => { // функия генерирования ключа
+  const key = [];
   let randomnum = 0;
-  const side = randomInteger( 1, 10 );    
-  for (let c = 0; c < side; c++) {
+  const side = randomInteger(1, 10);
+  for (let c = 0; c < side; c += 1) {
     randomnum = randomInteger(0, 99);
-    key.push( randomnum );
-}
+    key.push(randomnum);
+  }
   const result1 = key.join('');
   return result1;
 };
@@ -70,17 +70,17 @@ const codding = () => { // функция кодировки
   let newalphabet = alphabet;
   if (rad[1].checked) {
     const generatedkey = generatekey();// генерируем ключ
-    for (let j = 0; j < lenght; j++) {// создаём алфавит символов в тексте 
+    for (let j = 0; j < lenght; j += 1) { // создаём алфавит символов в тексте 
       alphabet[res[j]] = res[j];
     }
     for (const keys  of Object.keys(newalphabet)) { // генерируем рандомный символы для каждой буквы
       newalphabet[keys] = randomsymbol();
     }
-    for ( let i = 0; i < lenght; i++ ) { //кодируем наш текст
+    for (let i = 0; i < lenght; i += 1) { // кодируем наш текст
       for (const keys  of Object.keys(alphabet)) {
         if (res[i] == keys) {
           res[i] = newalphabet[keys];
-        } 
+        }
       }
     }
     res = res.join('');
@@ -93,10 +93,10 @@ const codding = () => { // функция кодировки
     text_out.appendChild(characterSpan);
   }
 
-//}
+  // }
 
-// вывод зашифрован текста
-// const uncodding = (key1, text) =>{ // функия раскодировки
+  // вывод зашифрован текста
+  // const uncodding = (key1, text) =>{ // функия раскодировки
   if (rad[0].checked) {
     if (key_input.value === result.key.name) {
       let resofuncoding = text_input1.value.split('');
@@ -122,4 +122,4 @@ btnforkey.onclick = function copykey() {
   navigator.clipboard.writeText(result.key.name);
 };
 
-document.querySelector('button').onclick = codding;
+this.document.querySelector('button').onclick = codding;
